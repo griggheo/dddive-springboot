914504eb1261fbf98d2e5e6ed77ae9698e7b8067
griggheo <grig@gheorghiu.net>
2022-10-09 20:53:07 -0700

    simple SpringBoot maven app

---
+++ src/main/java/io/codepraxis/dddivespringboot/DddiveSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.codepraxis.dddivespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddiveSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddiveSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/codepraxis/dddivespringboot/DddiveSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codepraxis.dddivespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddiveSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
